c34106fb730e2eca95a31341adbb7344d09a52b0
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-07 09:30:32 +0000

    [Start/Finish]3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
@@ -20,7 +21,7 @@ class BlogsController < ApplicationController
   
   private
   def blog_params
-    params.require(:blog).permit(:titlw, :category, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
   
 end
